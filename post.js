dfe1a8ae7d7b55128033292cf523966ccf795804
Gimhana78 <gimhandeshapriya222@gmail.com>
2023-01-14 06:57:28 +0530

    new2

--- post.js
+++ post.js
@@ -1,3 +1,6 @@
+"<script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js'>"+"</script>";
+"<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' rel='stylesheet'>"
+    "<link href='https://getbootstrap.com/docs/5.3/assets/css/docs.css' rel='stylesheet'>"+"</link>"
 let filterArray =[];
 const galleryArray=[
     {
@@ -42,14 +45,7 @@ const galleryArray=[
         releaseDate:"2023",
         desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, omnis commodi aliquid sapiente ab earum harum illo tempora ipsa. Tempora porro aspernatur aperiam in harum!"
     },
-    
-    // {
-    //     id:6,
-    //     name:"Test 06",
-    //     src:"./img/p1.jpg",
-    //     releaseDate:"2023",
-    //     desc:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque, omnis commodi aliquid sapiente ab earum harum illo tempora ipsa. Tempora porro aspernatur aperiam in harum!"
-    // },
+
 
     ];
 
@@ -67,10 +63,12 @@ function showGallery(showPost){
             "<img src='"+showPost[i].src+ "'class='mx-auto d-block img-fluid'/>"+
             "<p class='text-center'>"+showPost[i].desc+"</p>"+ 
             "<h6 class=' '>"+"Release Date : "+showPost[i].releaseDate+"</h6>"+
-            "<button type='button' class='btn btn-outline-info '>"+"More..."+"</button>"+
+           // "<button type='button' class='btn btn-outline-info '>"+"More..."+"</button>"+
+            
             "<hr class='mb-4'>"+"</div>";
         
     }
 }
 showGallery(galleryArray);
+
 
